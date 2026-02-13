2edd27a06725bcc9201761ed984675a0710becb1
Thai Nong <hoangthai9217@gmail.com>
2026-02-13 17:08:34 +0700

    feat(plane-ee): add random provider for telegram webhook secrets

---
+++ terraform/plane-ee/providers.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "generic-gha-terraform-state"
+    key          = "plane-ee/terraform.tfstate"
+    region       = "ap-southeast-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project   = "plane-ee"
+      ManagedBy = "terraform"
+    }
+  }
+}
